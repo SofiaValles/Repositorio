cf74d7c6bb4a6de463512b640711cbce233a37a1
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-08-21 23:36:01 -0300

    Update coeficiente de curtosis final, si anda bien.py

--- coeficiente de curtosis final, si anda bien.py
+++ coeficiente de curtosis final, si anda bien.py
@@ -6,9 +6,10 @@ def calcular_curtosis(datos):
 
     curtosis = sum((x - media) ** 4 for x in datos) / ((n - 1)* desviacion_estandar ** 4)
     
-    # Ajuste de Fisher para obtener la curtosis con sesgo corregido
+    #Restamos 3 para que la distribución normal tenga una curtosis de 0, ya que la curtosis de una distribución normal es exactamente 3
     curtosis_final =  curtosis - 3 
 
+    #Determinación del tipo de curtosis
     if curtosis_final > 0:
         tipo_curtosis= "Leptocúrtica"
     elif curtosis_final == 0:
@@ -20,6 +21,8 @@ def calcular_curtosis(datos):
     return curtosis_redondeada, tipo_curtosis
 
 # Ejemplo de uso
-datos = [-1000, 2, 38, 4000]
+datos = [1, 2, 3, 4]
+curtosis_redondeada, tipo_curtosis = calcular_curtosis(datos)
+print("El coeficiente de curtosis es: ",curtosis_redondeada," y según su resultado es: ", tipo_curtosis)
 curtosis_redondeada, tipo_curtosis = calcular_curtosis(datos)
 print("El coeficiente de curtosis es: ",curtosis_redondeada," y según su resultado es: ", tipo_curtosis)
